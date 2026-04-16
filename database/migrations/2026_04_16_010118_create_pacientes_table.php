11ceb7279ba5af7ff066eb8b86fe27b786c93f57
CHARLES-21PR <2301010144@undc.edu.pe>
2026-04-15 20:53:23 -0500

    table_bd_and_route_api

---
+++ database/migrations/2026_04_16_010118_create_pacientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('dni', 8)->unique();
+            $table->string('HistoriaClinica')->unique();
+            $table->string('telefono', 9)->nullable();
+            $table->string('email')->nullable()->unique();
+            $table->boolean('sis')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
